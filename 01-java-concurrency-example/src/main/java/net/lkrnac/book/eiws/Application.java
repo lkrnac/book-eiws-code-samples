ba67d6948d36ef744d172c9a550577e3ccf67d3b
lubos krnac <lubos.krnac@gmail.com>
2014-11-01 15:28:21 +0000

    Fixing incorrect awaitTermination

--- 01-java-concurrency-example/src/main/java/net/lkrnac/book/eiws/Application.java
+++ 01-java-concurrency-example/src/main/java/net/lkrnac/book/eiws/Application.java
@@ -9,23 +9,27 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class Application {
-    private static final int EXEC_COUNT = 10;
+	private static final int EXEC_COUNT = 10;
 
 	public static void main(String[] args) throws InterruptedException {
-    	ExecutorService executorService = Executors.newWorkStealingPool();
-    	List<Future<String>> results = new ArrayList<>(10);
+		ExecutorService executorService = Executors.newWorkStealingPool();
+		List<Future<String>> results = new ArrayList<>(10);
 
-    	for (int idx = 0; idx < EXEC_COUNT; idx++){
-    		SimpleTask task = new SimpleTask();
+		for (int idx = 0; idx < EXEC_COUNT; idx++) {
+			SimpleTask task = new SimpleTask();
 			results.add(executorService.submit(task));
-    	}
-    	//executorService.awaitTermination(0, TimeUnit.DAYS);
-    	for (Future<String> result : results){
-    		try {
+		}
+
+		//behavior would be practically same also without these two lines
+		executorService.shutdown();
+		executorService.awaitTermination(Integer.MAX_VALUE, TimeUnit.DAYS);
+
+		for (Future<String> result : results) {
+			try {
 				System.out.println(result.get());
 			} catch (InterruptedException | ExecutionException e) {
 				System.out.println(e.getLocalizedMessage());
 			}
-    	}
-    }
+		}
+	}
 }
