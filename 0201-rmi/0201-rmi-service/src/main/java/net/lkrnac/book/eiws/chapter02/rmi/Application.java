71a68133f457c223f050592822de7838bdf118a2
lubos krnac <lubos.krnac@gmail.com>
2014-11-30 14:39:50 +0000

    Change main class of RMI service

--- 0201-rmi/0201-rmi-service/src/main/java/net/lkrnac/book/eiws/chapter02/rmi/Application.java
+++ 0201-rmi/0201-rmi-service/src/main/java/net/lkrnac/book/eiws/chapter02/rmi/Application.java
@@ -1,14 +1,9 @@
 package net.lkrnac.book.eiws.chapter02.rmi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
-@Configuration
-@Import(ServiceConfiguration.class)
 public class Application {
-
   public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
+    SpringApplication.run(ServiceConfiguration.class, args);
   }
 }
