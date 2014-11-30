49392490d985f6506cafddd4ac40258708fed0d8
lubos krnac <lubos.krnac@gmail.com>
2014-11-30 17:25:00 +0000

    Simple test for RMI service context loading

--- 0201-rmi/0201-rmi-service/src/test/java/net/lkrnac/book/eiws/chapter02/rmi/ServiceConfigurationITest.java
+++ 0201-rmi/0201-rmi-service/src/test/java/net/lkrnac/book/eiws/chapter02/rmi/ServiceConfigurationITest.java
@@ -1,10 +1,11 @@
 package net.lkrnac.book.eiws.chapter02.rmi;
 
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 @ContextConfiguration(classes = ServiceConfiguration.class)
-public class ServiceConfigurationTests {
+public class ServiceConfigurationITest extends AbstractTestNGSpringContextTests {
   @Test
   public void contextLoads() {
   }
---
+++ 0201-rmi/0201-rmi-service/src/test/java/net/lkrnac/book/eiws/chapter02/rmi/ServiceXmlContextITest.java
@@ -0,0 +1,12 @@
+package net.lkrnac.book.eiws.chapter02.rmi;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(locations = "classpath:bar-service-context.xml")
+public class ServiceXmlContextITest extends AbstractTestNGSpringContextTests {
+  @Test
+  public void contextLoads() {
+  }
+}
