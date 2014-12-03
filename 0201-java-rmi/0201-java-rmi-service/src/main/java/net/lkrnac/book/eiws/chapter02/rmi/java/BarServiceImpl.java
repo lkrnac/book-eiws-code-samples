a6ff4f341c375a0a777449d134e17695cd6f2d4a
lubos krnac <lubos.krnac@gmail.com>
2014-12-03 21:47:40 +0000

    Tidy up Java rmi service example

--- 0201-java-rmi/0201-java-rmi-service/src/main/java/net/lkrnac/book/eiws/chapter02/rmi/java/BarServiceImpl.java
+++ 0201-java-rmi/0201-java-rmi-service/src/main/java/net/lkrnac/book/eiws/chapter02/rmi/java/BarServiceImpl.java
@@ -4,10 +4,10 @@ import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
 public class BarServiceImpl extends UnicastRemoteObject implements BarService {
+  private static final long serialVersionUID = 1L;
+
   protected BarServiceImpl() throws RemoteException {
   }
-
-  private static final long serialVersionUID = 1L;
 
   @Override
   public String serveBar(String param) throws RemoteException {
