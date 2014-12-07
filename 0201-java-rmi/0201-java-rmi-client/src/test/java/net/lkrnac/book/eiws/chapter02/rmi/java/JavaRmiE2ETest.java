81bcaebe7d648d4a50a9a5a02629ff20afc022d6
lubos krnac <lubos.krnac@gmail.com>
2014-12-07 15:30:51 +0000

    Fix typo in assertion

--- 0201-java-rmi/0201-java-rmi-client/src/test/java/net/lkrnac/book/eiws/chapter02/rmi/java/JavaRmiE2ETest.java
+++ 0201-java-rmi/0201-java-rmi-client/src/test/java/net/lkrnac/book/eiws/chapter02/rmi/java/JavaRmiE2ETest.java
@@ -19,7 +19,7 @@ public class JavaRmiE2ETest {
       Thread.sleep(3000);
 
       String response = new FooClient().callService("E2E test");
-      assertEquals(response, "Bar service reponse to parameter: E2E test");
+      assertEquals(response, "Bar service response to parameter: E2E test");
     } finally {
       process.destroyForcibly();
     }
