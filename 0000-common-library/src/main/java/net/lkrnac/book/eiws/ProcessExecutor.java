7fb5a057df74e7b1d3acf06660cf3a4ac4a21afd
lubos krnac <lubos.krnac@gmail.com>
2014-12-14 12:55:25 +0000

    Static analyzers rules applied

--- 0000-common-library/src/main/java/net/lkrnac/book/eiws/ProcessExecutor.java
+++ 0000-common-library/src/main/java/net/lkrnac/book/eiws/ProcessExecutor.java
@@ -4,15 +4,29 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
 
+/**
+ * Class responsible to executing jar files.
+ * 
+ * @author Lubos Krnac
+ *
+ */
 public class ProcessExecutor {
-  public Process execute(String jarName) throws IOException {
-    Process p = null;
-    ProcessBuilder pb = new ProcessBuilder("java", "-jar", jarName);
-    pb.directory(new File("target/dependency"));
-    File log = new File("log");
-    pb.redirectErrorStream(true);
-    pb.redirectOutput(Redirect.appendTo(log));
-    p = pb.start();
-    return p;
+  /**
+   * Runs executable jar is separate process and redirects console into "log"
+   * file.
+   * 
+   * @param jarName
+   *          name of the jar to run
+   * @return started {@link Process} instance
+   * @throws IOException
+   *           if I/O error occurs
+   */
+  public final Process execute(String jarName) throws IOException {
+    ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarName);
+    processBuilder.directory(new File("target/dependency"));
+    File log = new File("process-executor.log");
+    processBuilder.redirectErrorStream(true);
+    processBuilder.redirectOutput(Redirect.appendTo(log));
+    return processBuilder.start();
   }
 }
