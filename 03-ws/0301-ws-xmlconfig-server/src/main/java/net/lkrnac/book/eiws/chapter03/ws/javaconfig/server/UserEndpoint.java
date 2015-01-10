a539bb5e10a8782aa71db2eaca20efd73e66a587
lubos krnac <lubos.krnac@gmail.com>
2015-01-10 23:43:42 +0000

    Soap examples

---
+++ 03-ws/0301-ws-xmlconfig-server/src/main/java/net/lkrnac/book/eiws/chapter03/ws/javaconfig/server/UserEndpoint.java
@@ -0,0 +1,23 @@
+package net.lkrnac.book.eiws.chapter03.ws.javaconfig.server;
+
+import net.lkrnac.book.eiws.chapter03.ws.javaconfig.model.UserDetailsType;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class UserEndpoint {
+  @PayloadRoot(namespace = "http://lkrnac.net/book/eiws/chapter03/ws/javaconfig/model", localPart = "getUserDetails")
+  @ResponsePayload
+  public UserDetailsType getCountry(@RequestPayload String userEmail) {
+    UserDetailsType userDetails = null;
+    if ("lubos.krnac@gmail.com".equals(userEmail)) {
+      userDetails = new UserDetailsType();
+      userDetails.setFirstName("Lubos");
+      userDetails.setLastName("Krnac");
+    }
+    return userDetails;
+  }
+}
