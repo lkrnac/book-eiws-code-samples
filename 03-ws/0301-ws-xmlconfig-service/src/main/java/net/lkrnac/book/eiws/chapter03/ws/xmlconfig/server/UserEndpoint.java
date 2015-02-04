016018322a2034eafabc7a0c242ad95ba8475d00
lubos krnac <lubos.krnac@gmail.com>
2015-02-04 20:16:05 +0000

    Fixed service

--- 03-ws/0301-ws-xmlconfig-service/src/main/java/net/lkrnac/book/eiws/chapter03/ws/xmlconfig/server/UserEndpoint.java
+++ 03-ws/0301-ws-xmlconfig-service/src/main/java/net/lkrnac/book/eiws/chapter03/ws/xmlconfig/server/UserEndpoint.java
@@ -1,6 +1,7 @@
 package net.lkrnac.book.eiws.chapter03.ws.xmlconfig.server;
 
 import localhost._10301._0301_ws_xmlconfig_service.UserDetailsResponse;
+import localhost._10301._0301_ws_xmlconfig_service.UserRequest;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -8,16 +9,13 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
-// @Address("http://localhost:8080/StockService")
 public class UserEndpoint {
-  // @PayloadRoot(namespace =
-  // "http://lkrnac.net/book/eiws/chapter03/ws/xmlconfig/model", localPart =
-  // "getUserDetails")
-  @PayloadRoot(namespace = "http://localhost:10301/0301-ws-xmlconfig-service/", localPart = "getUserDetails")
+  @PayloadRoot(namespace = "http://localhost:10301/0301-ws-xmlconfig-service", localPart = "UserRequest")
   @ResponsePayload
-  public UserDetailsResponse getCountry(@RequestPayload String userEmail) {
+  public UserDetailsResponse getUserDetails(
+      @RequestPayload UserRequest userRequest) {
     UserDetailsResponse userDetails = null;
-    if ("lubos.krnac@gmail.com".equals(userEmail)) {
+    if ("lubos.krnac@gmail.com".equals(userRequest.getEmail())) {
       userDetails = new UserDetailsResponse();
       userDetails.setFirstName("Lubos");
       userDetails.setLastName("Krnac");
