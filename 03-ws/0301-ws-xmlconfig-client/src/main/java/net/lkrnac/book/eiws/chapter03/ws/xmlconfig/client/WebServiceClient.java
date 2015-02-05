97c374a7f3fec078327f9336d7ac27577a22e7aa
lubos krnac <lubos.krnac@gmail.com>
2015-02-05 23:00:17 +0000

    Test Client example

--- 03-ws/0301-ws-xmlconfig-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/xmlconfig/client/WebServiceClient.java
+++ 03-ws/0301-ws-xmlconfig-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/xmlconfig/client/WebServiceClient.java
@@ -1,25 +1,14 @@
 package net.lkrnac.book.eiws.chapter03.ws.xmlconfig.client;
 
-import javax.annotation.PostConstruct;
-
 import net.lkrnac.book.eiws.chapter03.ws.xmlconfig.model.UserDetailsResponse;
 import net.lkrnac.book.eiws.chapter03.ws.xmlconfig.model.UserRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 @Component
-@EnableAutoConfiguration
 public class WebServiceClient {
-  private static final Logger log = LoggerFactory
-      .getLogger(WebServiceClient.class);
-
-  private static final String URL =
-      "http://localhost:10301/0301-ws-xmlconfig-service/getUserDetails";
   private WebServiceTemplate webServiceTemplate;
 
   @Autowired
@@ -32,16 +21,7 @@ public class WebServiceClient {
     request.setEmail(email);
 
     UserDetailsResponse userDetails =
-        (UserDetailsResponse) webServiceTemplate.marshalSendAndReceive(URL,
-            request);
+        (UserDetailsResponse) webServiceTemplate.marshalSendAndReceive(request);
     return userDetails;
-  }
-
-  @PostConstruct
-  public void test() {
-    UserDetailsResponse userDetails =
-        this.getUserDetails("lubos.krnac@gmail.com");
-    log.error("User Details: " + userDetails.getFirstName() + " "
-        + userDetails.getLastName());
   }
 }
---
+++ 03-ws/0301-ws-xmlconfig-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/xmlconfig/config/WsXmlconfigClientApplication.java
@@ -0,0 +1,37 @@
+package net.lkrnac.book.eiws.chapter03.ws.xmlconfig.config;
+
+import javax.annotation.PostConstruct;
+
+import net.lkrnac.book.eiws.chapter03.ws.xmlconfig.client.WebServiceClient;
+import net.lkrnac.book.eiws.chapter03.ws.xmlconfig.model.UserDetailsResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:ws-client-config.xml")
+@EnableAutoConfiguration
+public class WsXmlconfigClientApplication {
+  private static final Logger log = LoggerFactory
+      .getLogger(WebServiceClient.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(WsXmlconfigClientApplication.class, args);
+  }
+
+  @Autowired
+  private WebServiceClient wsClient;
+
+  @PostConstruct
+  public void test() {
+    UserDetailsResponse userDetails =
+        wsClient.getUserDetails("lubos.krnac@gmail.com");
+    log.info("User Details: " + userDetails.getFirstName() + " "
+        + userDetails.getLastName());
+  }
+}
