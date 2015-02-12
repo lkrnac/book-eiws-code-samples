ac077a1616757b9fe4d5d9399888cce47b7648b2
lubos krnac <lubos.krnac@gmail.com>
2015-02-12 19:07:27 +0000

    Use Apache HttpClient in client example

--- 03-ws/0303-ws-boot-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/boot/client/WsBootClientConfiguration.java
+++ 03-ws/0303-ws-boot-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/boot/client/WsBootClientConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 
 @Configuration
 @ComponentScan
@@ -23,6 +24,12 @@ public class WsBootClientConfiguration {
     webServiceTemplate.setUnmarshaller(marshaller);
     webServiceTemplate
         .setDefaultUri("http://localhost:10303/0303-ws-boot-service");
+
+    HttpComponentsMessageSender messageSender =
+        new HttpComponentsMessageSender();
+    messageSender.setConnectionTimeout(100);
+    messageSender.setMaxTotalConnections(10);
+    webServiceTemplate.setMessageSender(messageSender);
     return webServiceTemplate;
   }
 }
