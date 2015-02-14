36f09b42bb0d99bff4ce69a6edf7cfc4d0b5a40a
lubos krnac <lubos.krnac@gmail.com>
2015-02-14 18:26:01 +0000

    Remove action callback

--- 03-ws/0303-ws-boot-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/boot/client/WebServiceClient.java
+++ 03-ws/0303-ws-boot-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/boot/client/WebServiceClient.java
@@ -5,21 +5,11 @@ import net.lkrnac.book.eiws.chapter03.ws.boot.model.UserRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.ws.WebServiceMessage;
-import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.soap.SoapMessage;
 
 @Component
 public class WebServiceClient {
   private WebServiceTemplate webServiceTemplate;
-
-  private static final WebServiceMessageCallback messageCallback =
-      new WebServiceMessageCallback() {
-        public void doWithMessage(WebServiceMessage message) {
-          ((SoapMessage) message).setSoapAction("getUserDetails");
-        }
-      };
 
   @Autowired
   public WebServiceClient(WebServiceTemplate webServiceTemplate) {
@@ -31,8 +21,7 @@ public class WebServiceClient {
     request.setEmail(email);
 
     UserDetailsResponse userDetails =
-        (UserDetailsResponse) webServiceTemplate.marshalSendAndReceive(request,
-            messageCallback);
+        (UserDetailsResponse) webServiceTemplate.marshalSendAndReceive(request);
     return userDetails;
   }
 }
