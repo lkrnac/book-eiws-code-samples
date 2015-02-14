33eb9180803b2fd23462e3694cee69f196d13ada
lubos krnac <lubos.krnac@gmail.com>
2015-02-14 18:25:04 +0000

    Enhance action callback

--- 03-ws/0302-ws-javaconfig-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/javaconfig/client/WebServiceClient.java
+++ 03-ws/0302-ws-javaconfig-client/src/main/java/net/lkrnac/book/eiws/chapter03/ws/javaconfig/client/WebServiceClient.java
@@ -5,21 +5,15 @@ import net.lkrnac.book.eiws.chapter03.ws.javaconfig.model.UserRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.ws.WebServiceMessage;
-import org.springframework.ws.client.core.WebServiceMessageCallback;
 import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.soap.SoapMessage;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 
 @Component
 public class WebServiceClient {
   private WebServiceTemplate webServiceTemplate;
 
-  private static final WebServiceMessageCallback messageCallback =
-      new WebServiceMessageCallback() {
-        public void doWithMessage(WebServiceMessage message) {
-          ((SoapMessage) message).setSoapAction("getUserDetails");
-        }
-      };
+  private static final SoapActionCallback messageCallback =
+      new SoapActionCallback("getUserDetails");
 
   @Autowired
   public WebServiceClient(WebServiceTemplate webServiceTemplate) {
