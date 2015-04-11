71cf029d2bb850d748e3a39000ef25367e77721f
lubos krnac <lubos.krnac@gmail.com>
2015-04-11 11:24:42 +0100

    RestController usage

--- 04-rest/0405-rest-restcontroller-server/src/main/java/net/lkrnac/book/eiws/chapter04/restcontroller/UserController.java
+++ 04-rest/0405-rest-restcontroller-server/src/main/java/net/lkrnac/book/eiws/chapter04/restcontroller/UserController.java
@@ -10,17 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Controller
+@RestController
 @RequestMapping(UrlConstants.USERS_URL)
 public class UserController {
   private final UserRepository userRepository;
@@ -32,13 +31,11 @@ public class UserController {
   }
 
   @RequestMapping(method = RequestMethod.GET)
-  @ResponseBody
   public Collection<User> getUsers() {
     return userRepository.getAllUsers();
   }
 
   @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-  @ResponseBody
   public User getUser(@PathVariable("id") int identifier) {
     return userRepository.getUser(identifier);
   }
