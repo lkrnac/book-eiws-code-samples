15ce13284fd207cd127031a0e61d1a6927cd097f
lubos krnac <lubos.krnac@gmail.com>
2015-05-04 19:09:47 +0100

    Spring JMS example first draft

---
+++ 0504-spring-jms-xml/src/test/java/net/lkrnac/book/eiws/chapter05/JavaJmsAsyncApplicationTests.java
@@ -0,0 +1,16 @@
+package net.lkrnac.book.eiws.chapter05;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JavaJmsAsyncApplication.class)
+public class JavaJmsAsyncApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
