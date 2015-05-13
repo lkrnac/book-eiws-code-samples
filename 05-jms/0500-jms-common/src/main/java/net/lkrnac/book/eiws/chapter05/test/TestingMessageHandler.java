6fa592603e5417da42fdc076f9dd0c7a5f3ce7fc
lubos krnac <lubos.krnac@gmail.com>
2015-05-13 22:43:30 +0100

    Tidy up testing message handler

--- 05-jms/0500-jms-common/src/main/java/net/lkrnac/book/eiws/chapter05/test/TestingMessageHandler.java
+++ 05-jms/0500-jms-common/src/main/java/net/lkrnac/book/eiws/chapter05/test/TestingMessageHandler.java
@@ -4,22 +4,18 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 public class TestingMessageHandler implements SimpleMessageHandler {
-  private final BlockingQueue queue = new ArrayBlockingQueue<>(10);
-  private String message;
+  private final BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
 
   @Override
   public void handleMessage(String message) {
-    this.message = message;
     queue.add(message);
   }
 
   public String getMessage() {
     try {
-      return (String) queue.take();
+      return queue.take();
     } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      throw new IllegalStateException(e);
     }
-    return null;
   }
 }
