c93fc8bd0f39ea132a7b52d3d61fa244d3f4ed08
lubos krnac <lubos.krnac@gmail.com>
2015-05-16 12:50:54 +0100

    Test for JMS sync example

--- 05-jms/0507-sync-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/SimpleMessageReader.java
+++ 05-jms/0507-sync-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/SimpleMessageReader.java
@@ -1,7 +1,5 @@
 package net.lkrnac.book.eiws.chapter05;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,18 +8,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class SimpleMessageReader {
   private JmsTemplate jmsTemplate;
-  private static final Logger LOG = LoggerFactory
-      .getLogger(SimpleMessageReader.class);
+  private SimpleMessageHandler simpleMessageHandler;
 
   @Autowired
-  public SimpleMessageReader(JmsTemplate jmsTemplate) {
+  public SimpleMessageReader(JmsTemplate jmsTemplate,
+      SimpleMessageHandler simpleMessageHandler) {
     super();
     this.jmsTemplate = jmsTemplate;
+    this.simpleMessageHandler = simpleMessageHandler;
   }
 
   @Scheduled(fixedRate = 1200L)
   public void readMessage() {
     String message = (String) jmsTemplate.receiveAndConvert("messageQueue");
-    LOG.info("Received message: {}", message);
+    simpleMessageHandler.handleMessage(message);
   }
 }
--- 05-jms/0507-sync-jms-java-config/src/test/java/net/lkrnac/book/eiws/chapter05/JavaConfigJmsSyncApplicationTests.java
+++ 05-jms/0507-sync-jms-java-config/src/test/java/net/lkrnac/book/eiws/chapter05/JavaConfigJmsSyncApplicationTests.java
@@ -1,8 +1,11 @@
 package net.lkrnac.book.eiws.chapter05;
 
 import net.lkrnac.book.eiws.chapter05.test.CommonJmsTest;
+import net.lkrnac.book.eiws.chapter05.test.CommonJmsTest.TestConfiguration;
 
-//@SpringApplicationConfiguration(classes = { JavaConfigJmsSyncApplication.class,
-//    TestConfiguration.class })
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+@SpringApplicationConfiguration(classes = { JavaConfigJmsSyncApplication.class,
+    TestConfiguration.class })
 public class JavaConfigJmsSyncApplicationTests extends CommonJmsTest {
 }
