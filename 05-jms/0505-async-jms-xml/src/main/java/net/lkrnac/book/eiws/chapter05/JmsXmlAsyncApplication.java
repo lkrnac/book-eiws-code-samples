6af67508592cf43a9dabaeef83405ebc1ead9bbb
lubos krnac <lubos.krnac@gmail.com>
2015-05-16 12:25:31 +0100

    Tidy up scheduling

--- 05-jms/0505-async-jms-xml/src/main/java/net/lkrnac/book/eiws/chapter05/JmsXmlAsyncApplication.java
+++ 05-jms/0505-async-jms-xml/src/main/java/net/lkrnac/book/eiws/chapter05/JmsXmlAsyncApplication.java
@@ -1,25 +1,20 @@
 package net.lkrnac.book.eiws.chapter05;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
+@Slf4j
 @Configuration
 @ComponentScan
-@ImportResource("classpath:spring-jms.xml")
 @EnableScheduling
+@ImportResource("classpath:spring-jms.xml")
 public class JmsXmlAsyncApplication {
-  private static final String SIMPLE_MESSAGE = "simple message";
-  private static final Logger LOG = LoggerFactory
-      .getLogger(JmsXmlAsyncApplication.class);
-
   public static void main(String[] args) throws InterruptedException {
     SpringApplication.run(JmsXmlAsyncApplication.class, args);
   }
@@ -29,17 +24,8 @@ public class JmsXmlAsyncApplication {
     return new SimpleMessageHandler() {
       @Override
       public void handleMessage(String message) {
-        LOG.info("Message Received: {}", message);
+        log.info("Message Received: {}", message);
       }
     };
-  }
-
-  @Autowired
-  private SimpleMessageSender messageSender;
-
-  @Scheduled(fixedRate = 1000L)
-  public void sendMessage() throws InterruptedException {
-    LOG.info("Sending message: {}", SIMPLE_MESSAGE);
-    messageSender.send(SIMPLE_MESSAGE);
   }
 }
