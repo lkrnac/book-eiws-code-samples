80120a67358ac65e2aaa63f0ad2ae94a64a31fd9
lubos krnac <lubos.krnac@gmail.com>
2015-05-16 13:30:21 +0100

    JMS message converter example kick off

---
+++ 05-jms/0509-jms-message-converter/src/test/java/net/lkrnac/book/eiws/chapter05/JmsMessageConverterApplicationTests.java
@@ -0,0 +1,11 @@
+package net.lkrnac.book.eiws.chapter05;
+
+import net.lkrnac.book.eiws.chapter05.test.CommonJmsTest;
+import net.lkrnac.book.eiws.chapter05.test.CommonJmsTest.TestConfiguration;
+
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+@SpringApplicationConfiguration(classes = { JmsMessageConverterApplication.class,
+    TestConfiguration.class })
+public class JmsMessageConverterApplicationTests extends CommonJmsTest {
+}
