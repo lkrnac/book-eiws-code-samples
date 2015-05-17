8a333c87c937d172fb37cf1dd8af736ba9124bf7
lubos krnac <lubos.krnac@gmail.com>
2015-05-17 11:45:49 +0100

    ChachingConnectionFactory example

--- 05-jms/0511-jms-caching-connection-factory/src/main/java/net/lkrnac/book/eiws/chapter05/JmsConfiguration.java
+++ 05-jms/0511-jms-caching-connection-factory/src/main/java/net/lkrnac/book/eiws/chapter05/JmsConfiguration.java
@@ -2,36 +2,23 @@ package net.lkrnac.book.eiws.chapter05;
 
 import javax.jms.ConnectionFactory;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
+import org.springframework.jms.connection.CachingConnectionFactory;
 
 @Configuration
 @EnableJms
 public class JmsConfiguration {
-  // @Bean
-  // public ActiveMQConnectionFactory connectionFactory() {
-  // ActiveMQConnectionFactory connectionFactory =
-  // new ActiveMQConnectionFactory();
-  // connectionFactory.setBrokerURL("tcp://localhost:61616");
-  // return connectionFactory;
-  // }
-  //
-  // @Bean
-  // public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
-  // return new JmsTemplate(connectionFactory);
-  // }
-
   @Bean
-  public DefaultMessageListenerContainer defaultMessageListenerContainer(
-      ConnectionFactory connectionFactory,
-      SimpleMessageListener simpleMessageListener) {
-    DefaultMessageListenerContainer listenerContainer =
-        new DefaultMessageListenerContainer();
-    listenerContainer.setConnectionFactory(connectionFactory);
-    listenerContainer.setDestinationName("messageQueue");
-    listenerContainer.setMessageListener(simpleMessageListener);
-    return listenerContainer;
+  public ConnectionFactory connectionFactory() {
+    ActiveMQConnectionFactory activeMqConnectionFactory =
+        new ActiveMQConnectionFactory();
+    activeMqConnectionFactory.setBrokerURL("vm://localhost");
+
+    CachingConnectionFactory connectionFactory =
+        new CachingConnectionFactory(activeMqConnectionFactory);
+    return connectionFactory;
   }
 }
