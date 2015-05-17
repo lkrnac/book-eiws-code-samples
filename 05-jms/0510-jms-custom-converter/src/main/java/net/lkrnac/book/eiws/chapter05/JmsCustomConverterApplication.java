01dd7b492022c0a49c9d4d965f314d49482bc78a
lubos krnac <lubos.krnac@gmail.com>
2015-05-17 19:25:02 +0100

    Tody up spring configs second commit

--- 05-jms/0510-jms-custom-converter/src/main/java/net/lkrnac/book/eiws/chapter05/JmsCustomConverterApplication.java
+++ 05-jms/0510-jms-custom-converter/src/main/java/net/lkrnac/book/eiws/chapter05/JmsCustomConverterApplication.java
@@ -1,15 +1,10 @@
 package net.lkrnac.book.eiws.chapter05;
 
-import javax.jms.ConnectionFactory;
-
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
@@ -18,24 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class JmsCustomConverterApplication {
   public static void main(String[] args) throws InterruptedException {
     SpringApplication.run(JmsCustomConverterApplication.class, args);
-  }
-
-  @Bean
-  public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory,
-      MessageConverter messageConverter) {
-    JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
-    jmsTemplate.setMessageConverter(messageConverter);
-    return jmsTemplate;
-  }
-
-  @Bean
-  public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
-      ConnectionFactory connectionFactory, MessageConverter messageConverter) {
-    DefaultJmsListenerContainerFactory factory =
-        new DefaultJmsListenerContainerFactory();
-    factory.setConnectionFactory(connectionFactory);
-    factory.setMessageConverter(messageConverter);
-    return factory;
   }
 
   @Bean
