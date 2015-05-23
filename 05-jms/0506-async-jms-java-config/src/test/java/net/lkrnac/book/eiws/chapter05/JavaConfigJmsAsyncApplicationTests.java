7e45462a3633ee76aa325aee6346f335d16d50e4
lubos krnac <lubos.krnac@gmail.com>
2015-05-23 13:01:02 +0100

    Start embedded broker for test also

--- 05-jms/0506-async-jms-java-config/src/test/java/net/lkrnac/book/eiws/chapter05/JavaConfigJmsAsyncApplicationTests.java
+++ 05-jms/0506-async-jms-java-config/src/test/java/net/lkrnac/book/eiws/chapter05/JavaConfigJmsAsyncApplicationTests.java
@@ -3,9 +3,20 @@ package net.lkrnac.book.eiws.chapter05;
 import net.lkrnac.book.eiws.chapter05.test.simplemessage.CommonJmsSimpleMessageTest;
 import net.lkrnac.book.eiws.chapter05.test.simplemessage.CommonJmsSimpleMessageTest.TestSimpleMessageConfiguration;
 
+import org.apache.activemq.broker.BrokerService;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 
-@SpringApplicationConfiguration(classes = { JavaConfigJmsAsyncApplication.class,
-    TestSimpleMessageConfiguration.class })
-public class JavaConfigJmsAsyncApplicationTests extends CommonJmsSimpleMessageTest {
+@SpringApplicationConfiguration(classes = {
+    JavaConfigJmsAsyncApplication.class, TestSimpleMessageConfiguration.class })
+public class JavaConfigJmsAsyncApplicationTests extends
+    CommonJmsSimpleMessageTest {
+  {
+    try {
+      BrokerService broker = new BrokerService();
+      broker.addConnector("tcp://localhost:10506");
+      broker.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
