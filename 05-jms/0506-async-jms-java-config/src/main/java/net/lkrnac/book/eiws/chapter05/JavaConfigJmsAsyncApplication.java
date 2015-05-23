9ca2f142f421fa2820b1b8f13cd13256e1615001
lubos krnac <lubos.krnac@gmail.com>
2015-05-23 12:59:23 +0100

    Use java config without @EnableAutoConfiguration

--- 05-jms/0506-async-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/JavaConfigJmsAsyncApplication.java
+++ 05-jms/0506-async-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/JavaConfigJmsAsyncApplication.java
@@ -2,16 +2,21 @@ package net.lkrnac.book.eiws.chapter05;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.activemq.broker.BrokerService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
-@SpringBootApplication
+@ComponentScan
 @EnableScheduling
 public class JavaConfigJmsAsyncApplication {
-  public static void main(String[] args) throws InterruptedException {
+  public static void main(String[] args) throws Exception {
+    BrokerService broker = new BrokerService();
+    broker.addConnector("tcp://localhost:10506");
+    broker.start();
+
     SpringApplication.run(JavaConfigJmsAsyncApplication.class, args);
   }
 
--- 05-jms/0506-async-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/JmsConfiguration.java
+++ 05-jms/0506-async-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/JmsConfiguration.java
@@ -2,14 +2,26 @@ package net.lkrnac.book.eiws.chapter05;
 
 import javax.jms.ConnectionFactory;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @Configuration
 @EnableJms
 public class JmsConfiguration {
+  @Bean
+  public ConnectionFactory connectionFactory() {
+    return new ActiveMQConnectionFactory("tcp://localhost:10506");
+  }
+
+  @Bean
+  public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
+    return new JmsTemplate(connectionFactory);
+  }
+
   @Bean
   public DefaultMessageListenerContainer defaultMessageListenerContainer(
       ConnectionFactory connectionFactory,
