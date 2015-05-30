4b64e3e1b2cf96685be1e154f1e02dcd807b915e
lubos krnac <lubos.krnac@gmail.com>
2015-05-30 20:47:03 +0100

    Remove unnecessary destination

--- 05-jms/0515-publish-subscribe/src/main/java/net/lkrnac/book/eiws/chapter05/pubsub/JmsPublishSubscribeApplication.java
+++ 05-jms/0515-publish-subscribe/src/main/java/net/lkrnac/book/eiws/chapter05/pubsub/JmsPublishSubscribeApplication.java
@@ -1,9 +1,7 @@
 package net.lkrnac.book.eiws.chapter05.pubsub;
 
-import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -11,10 +9,5 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class JmsPublishSubscribeApplication {
   public static void main(String[] args) throws InterruptedException {
     SpringApplication.run(JmsPublishSubscribeApplication.class, args);
-  }
-
-  @Bean
-  public ActiveMQTopic simpleTopic() {
-    return new ActiveMQTopic("simpleTopic");
   }
 }
