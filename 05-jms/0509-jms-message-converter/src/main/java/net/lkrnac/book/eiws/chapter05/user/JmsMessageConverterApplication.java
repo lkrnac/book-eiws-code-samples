ee21b8252543a8c3d2e2b6e26324d750a3ce787c
lubos krnac <lubos.krnac@gmail.com>
2015-05-30 17:34:21 +0100

    Remove some rubbish after refactoring

--- 05-jms/0509-jms-message-converter/src/main/java/net/lkrnac/book/eiws/chapter05/user/JmsMessageConverterApplication.java
+++ 05-jms/0509-jms-message-converter/src/main/java/net/lkrnac/book/eiws/chapter05/user/JmsMessageConverterApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
 public class JmsMessageConverterApplication {
   public static void main(String[] args) throws InterruptedException {
     SpringApplication.run(JmsMessageConverterApplication.class, args);
--- 05-jms/0513-jms-messaging-template/src/main/java/net/lkrnac/book/eiws/chapter05/userwithrole/JmsMessagingTemplateApplication.java
+++ 05-jms/0513-jms-messaging-template/src/main/java/net/lkrnac/book/eiws/chapter05/userwithrole/JmsMessagingTemplateApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableScheduling
+@SpringBootApplication
 public class JmsMessagingTemplateApplication {
   public static void main(String[] args) throws InterruptedException {
     SpringApplication.run(JmsMessagingTemplateApplication.class, args);
