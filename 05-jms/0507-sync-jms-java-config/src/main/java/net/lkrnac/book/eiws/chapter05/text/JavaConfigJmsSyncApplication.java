abdf8330ba4156f4feaf584c5fe8ee7578602073
lubos krnac <lubos.krnac@gmail.com>
2015-05-30 14:45:59 +0100

    Fix sync JMS example

--- 05-jms/0507-sync-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/text/JavaConfigJmsSyncApplication.java
+++ 05-jms/0507-sync-jms-java-config/src/main/java/net/lkrnac/book/eiws/chapter05/text/JavaConfigJmsSyncApplication.java
@@ -2,7 +2,9 @@ package net.lkrnac.book.eiws.chapter05.text;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class JavaConfigJmsSyncApplication {
   public static void main(String[] args) throws InterruptedException {
