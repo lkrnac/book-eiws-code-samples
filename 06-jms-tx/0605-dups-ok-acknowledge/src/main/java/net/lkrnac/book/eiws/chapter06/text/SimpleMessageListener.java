3b1fcb1a0159fa687b86b63f25dfcc26ad143099
lubos krnac <lubos.krnac@gmail.com>
2015-06-01 17:25:05 +0100

    DUPS_OK_ACKNOWLEDGE example

---
+++ 06-jms-tx/0605-dups-ok-acknowledge/src/main/java/net/lkrnac/book/eiws/chapter06/text/SimpleMessageListener.java
@@ -0,0 +1,23 @@
+package net.lkrnac.book.eiws.chapter06.text;
+
+import javax.jms.JMSException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleMessageListener {
+  private SimpleService simpleService;
+
+  @Autowired
+  public SimpleMessageListener(SimpleService simpleService) {
+    super();
+    this.simpleService = simpleService;
+  }
+
+  @JmsListener(destination = "messageQueue")
+  public void readMessage(String messageText) throws JMSException {
+    simpleService.processText(messageText);
+  }
+}
---
+++ 06-jms-tx/0605-dups-ok-acknowledge/src/test/java/net/lkrnac/book/eiws/chapter06/text/DupsOkAckApplicationTests.java
@@ -0,0 +1,33 @@
+package net.lkrnac.book.eiws.chapter06.text;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = DupsOkAckApplication.class)
+public class DupsOkAckApplicationTests extends AbstractTestNGSpringContextTests {
+  private static final String SELECT_COUNT =
+      "select count(*) from TEXT_TABLE where text = ?";
+
+  private static final String MESSAGE_TEXT = "simple message";
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Test(timeOut = 3000)
+  public void testJms() throws InterruptedException {
+    // GIVEN: Spring configuration
+
+    // WHEN
+    Thread.sleep(500);
+
+    // THEN
+    long count =
+        jdbcTemplate.queryForObject(SELECT_COUNT, Long.class, MESSAGE_TEXT);
+    Assert.assertTrue(count > 0);
+  }
+
+}
