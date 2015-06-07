a4fb210fcbc522e11ef502e871f419faddfdc101
lubos krnac <lubos.krnac@gmail.com>
2015-06-07 14:57:09 +0100

    No need to have recurring sending

--- 06-jms-tx/0603-auto-acknowledge-async-success/src/main/java/net/lkrnac/book/eiws/chapter06/text/SimpleMessageSender.java
+++ 06-jms-tx/0603-auto-acknowledge-async-success/src/main/java/net/lkrnac/book/eiws/chapter06/text/SimpleMessageSender.java
@@ -1,10 +1,11 @@
 package net.lkrnac.book.eiws.chapter06.text;
 
+import javax.annotation.PostConstruct;
+
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -19,7 +20,7 @@ public class SimpleMessageSender {
     this.jmsTemplate = jmsTemplate;
   }
 
-  @Scheduled(fixedRate = 1000)
+  @PostConstruct
   public void send() {
     log.info("Sending message: {}", SIMPLE_MESSAGE);
     jmsTemplate.convertAndSend("messageQueue", SIMPLE_MESSAGE);
