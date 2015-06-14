8caa586f5902271835ec487736c584ce26f1dc98
lubos krnac <lubos.krnac@gmail.com>
2015-06-14 14:28:04 +0100

    Remove unnecesary configuration

--- 06-jms-tx/0608-client-acknowledge-listener-duplicate/src/main/java/net/lkrnac/book/eiws/chapter06/text/JmsConfiguration.java
+++ 06-jms-tx/0608-client-acknowledge-listener-duplicate/src/main/java/net/lkrnac/book/eiws/chapter06/text/JmsConfiguration.java
@@ -5,20 +5,10 @@ import javax.jms.Session;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
 
 @Configuration
-@EnableJms
 public class JmsConfiguration {
-  @Bean
-  public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory) {
-    JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
-    jmsTemplate.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
-    return jmsTemplate;
-  }
-
   @Bean
   public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
       ConnectionFactory connectionFactory) {
