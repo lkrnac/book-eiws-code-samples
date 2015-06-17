4af70c113c8381eac62132ee2992d7352cfd8687
lubos krnac <lubos.krnac@gmail.com>
2015-06-17 22:42:28 +0100

    Switch to HornetQ as much as possible

--- 05-jms/0508-async-listener-annotation/src/test/java/net/lkrnac/book/eiws/chapter05/text/ListenerAnnotationJmsAsyncApplicationTests.java
+++ 05-jms/0508-async-listener-annotation/src/test/java/net/lkrnac/book/eiws/chapter05/text/ListenerAnnotationJmsAsyncApplicationTests.java
@@ -1,6 +1,5 @@
 package net.lkrnac.book.eiws.chapter05.text;
 
-import net.lkrnac.book.eiws.chapter05.text.ListenerAnnotationJmsAsyncApplication;
 import net.lkrnac.book.eiws.chapter05.text.test.CommonJmsSimpleMessageTest;
 
 import org.springframework.boot.test.SpringApplicationConfiguration;
