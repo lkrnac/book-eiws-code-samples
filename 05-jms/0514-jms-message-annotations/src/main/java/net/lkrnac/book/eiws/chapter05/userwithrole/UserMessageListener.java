14a787ec3ebb24984469c05103b100d5d9c91fd0
lubos krnac <lubos.krnac@gmail.com>
2015-06-20 16:06:16 +0100

    Include @Headers annotation

--- 05-jms/0514-jms-message-annotations/src/main/java/net/lkrnac/book/eiws/chapter05/userwithrole/UserMessageListener.java
+++ 05-jms/0514-jms-message-annotations/src/main/java/net/lkrnac/book/eiws/chapter05/userwithrole/UserMessageListener.java
@@ -1,13 +1,18 @@
 package net.lkrnac.book.eiws.chapter05.userwithrole;
 
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
 import net.lkrnac.book.eiws.chapter05.user.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class UserMessageListener {
   private UserWithRoleService userWithRoleService;
@@ -19,7 +24,9 @@ public class UserMessageListener {
   }
 
   @JmsListener(destination = "queue/ExpiryQueue")
-  public void readMessage(@Payload User user, @Header String role) {
+  public void readMessage(@Payload User user, @Header String role,
+      @Headers Map<String, Object> headers) {
+    log.info("Message with ID " + headers.get("id") + " received");
     userWithRoleService.processUser(user, role);
   }
 }
