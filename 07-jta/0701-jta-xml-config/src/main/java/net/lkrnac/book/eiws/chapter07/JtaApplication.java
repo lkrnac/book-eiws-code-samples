0298c70dd15be891f4f91dd5d025805348ef69c5
lubos krnac <lubos.krnac@gmail.com>
2015-07-18 16:48:03 +0100

    Edists from the book

--- 07-jta/0701-jta-xml-config/src/main/java/net/lkrnac/book/eiws/chapter07/JtaApplication.java
+++ 07-jta/0701-jta-xml-config/src/main/java/net/lkrnac/book/eiws/chapter07/JtaApplication.java
@@ -6,13 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan
 @EnableJms
 @EnableScheduling
-@EnableTransactionManagement(proxyTargetClass = false)
 @ImportResource(value = { "classpath:spring-jdbc-config.xml",
     "classpath:spring-jta-config.xml", "classpath:spring-jms-config.xml" })
 public class JtaApplication {
