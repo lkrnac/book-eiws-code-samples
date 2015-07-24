0fb4a5a32a7e5a38566eb055c70f8d806c1cd1c3
lubos krnac <lubos.krnac@gmail.com>
2015-07-25 00:09:49 +0100

    Channel adapter with Java config

---
+++ 08-integration/080x-channel-adapter-generic-javaconfig/src/test/java/net/lkrnac/book/eiws/chapter08/SiApplicationTests.java
@@ -0,0 +1,28 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = SiApplication.class)
+public class SiApplicationTests extends AbstractTestNGSpringContextTests {
+  private static final String MESSAGE_TEXT = "simple message";
+
+  @Autowired
+  private WriteRepository writeRepository;
+
+  @Test(timeOut = 3000)
+  public void testSi() throws InterruptedException {
+    // GIVEN: Spring configuration
+
+    // WHEN
+
+    // THEN
+    TestWriteRepository testWriteService =
+        (TestWriteRepository) writeRepository;
+    Assert.assertEquals(testWriteService.getMessage(), MESSAGE_TEXT);
+    Assert.assertEquals(testWriteService.getMessage(), MESSAGE_TEXT);
+  }
+}
