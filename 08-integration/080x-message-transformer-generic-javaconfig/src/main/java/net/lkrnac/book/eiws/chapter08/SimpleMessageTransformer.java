b0b775218c43425f0c19cdd857e884dd0c1ff3ee
lubos krnac <lubos.krnac@gmail.com>
2015-07-28 17:59:20 +0100

    Generic message transformer with Java configuration example

---
+++ 08-integration/080x-message-transformer-generic-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SimpleMessageTransformer.java
@@ -0,0 +1,12 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Transformer;
+
+@MessageEndpoint
+public class SimpleMessageTransformer {
+  @Transformer(inputChannel = "inChannel", outputChannel = "transformedChannel")
+  public String transformMessage(String message) {
+    return new String("Message: " + message);
+  }
+}
---
+++ 08-integration/080x-message-transformer-generic-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/WriteService.java
@@ -0,0 +1,21 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WriteService {
+  private WriteRepository writeRepository;
+
+  @Autowired
+  public WriteService(WriteRepository writeRepository) {
+    super();
+    this.writeRepository = writeRepository;
+  }
+
+  @ServiceActivator(inputChannel = "transformedChannel")
+  public boolean writeAndIndicateSuccess(String message) {
+    return writeRepository.write(message) == 1;
+  }
+}
