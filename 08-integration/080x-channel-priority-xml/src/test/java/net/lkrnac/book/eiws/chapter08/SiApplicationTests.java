f499b75d631c950b0118bb40a43b34739203404a
lubos krnac <lubos.krnac@gmail.com>
2015-08-12 22:04:02 +0100

    Priority channel example

---
+++ 08-integration/080x-channel-priority-xml/src/test/java/net/lkrnac/book/eiws/chapter08/SiApplicationTests.java
@@ -0,0 +1,35 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = SiApplication.class)
+public class SiApplicationTests extends AbstractTestNGSpringContextTests {
+  {
+    System.setProperty("spring.profiles.active", "integration-test");
+  }
+
+  @Autowired
+  private WriteRepository writeRepository;
+
+  @Autowired
+  private SimpleService simpleService;
+
+  @Test
+  public void testSi() {
+    // GIVEN
+
+    // WHEN
+    simpleService.processText("message1");
+    simpleService.processText("message2");
+
+    // THEN
+    TestWriteRepository testWriteService =
+        (TestWriteRepository) writeRepository;
+    Assert.assertEquals(testWriteService.getMessage(), "message2");
+    Assert.assertEquals(testWriteService.getMessage(), "message1");
+  }
+}
