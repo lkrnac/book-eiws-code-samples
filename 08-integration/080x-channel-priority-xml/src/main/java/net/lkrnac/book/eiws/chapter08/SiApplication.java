28fa9533b6008b901f988c5020cc74143fa11abb
lubos krnac <lubos.krnac@gmail.com>
2015-08-13 21:29:43 +0100

    Make main class of PriorityChannel example consistent with other channel examples

--- 08-integration/080x-channel-priority-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/080x-channel-priority-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -1,18 +1,26 @@
 package net.lkrnac.book.eiws.chapter08;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
+@Slf4j
 @SpringBootApplication
 @ImportResource("classpath:si-config.xml")
 public class SiApplication {
+  private static final String MESSAGE2 = "message2";
+  private static final String MESSAGE1 = "message1";
+
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
 
     SimpleService simpleService = ctx.getBean(SimpleService.class);
-    simpleService.processText("message1");
-    simpleService.processText("message2");
+    simpleService.processText(MESSAGE1);
+    log.info(MESSAGE1 + " sent");
+    simpleService.processText(MESSAGE2);
+    log.info(MESSAGE2 + " sent");
   }
 }
