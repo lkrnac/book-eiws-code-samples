9aebeaa32452c02805753f9bdbf9e52881c6baaf
lubos krnac <lubos.krnac@gmail.com>
2015-08-13 22:57:11 +0100

    Long polling example

---
+++ 08-integration/080x-long-polling-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SimpleMessageComparator.java
@@ -0,0 +1,16 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import java.util.Comparator;
+
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleMessageComparator implements Comparator<Message<String>> {
+
+  @Override
+  public int compare(Message<String> o1, Message<String> o2) {
+    return -1 * o1.getPayload().compareTo(o2.getPayload());
+  }
+
+}
