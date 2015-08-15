0fa7c8485a38d3b127cb9729ca53be7b70db2850
lubos krnac <lubos.krnac@gmail.com>
2015-08-15 21:04:49 +0100

    BridgeTo annotation example

---
+++ 08-integration/080x-bridge-to-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -0,0 +1,15 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SiApplication {
+  public static void main(String[] args) throws InterruptedException {
+    ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
+
+    SimpleService simpleService = ctx.getBean(SimpleService.class);
+    simpleService.processText("Hi, Spring Integration");
+  }
+}
---
+++ 08-integration/080x-bridge-to-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiConfiguration.java
@@ -0,0 +1,26 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.BridgeTo;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
+
+@Configuration
+@IntegrationComponentScan
+public class SiConfiguration {
+  @Bean
+  @BridgeTo(value = "outChannel", poller = @Poller(fixedDelay = "100"))
+  public PollableChannel inChannel() {
+    return new QueueChannel();
+  }
+
+  @Bean
+  public MessageChannel outChannel() {
+    return new DirectChannel();
+  }
+}
