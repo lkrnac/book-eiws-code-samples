fbe1b42f00e215f9fa715d54f12032210af272ab
lubos krnac <lubos.krnac@gmail.com>
2015-08-15 21:02:59 +0100

    BridgeFrom annotation example

---
+++ 08-integration/080x-bridge-from-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiConfiguration.java
@@ -0,0 +1,26 @@
+package net.lkrnac.book.eiws.chapter08;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.BridgeFrom;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
+
+@Configuration
+@IntegrationComponentScan
+public class SiConfiguration {
+  @Bean
+  public PollableChannel inChannel() {
+    return new QueueChannel();
+  }
+
+  @Bean
+  @BridgeFrom(value = "inChannel", poller = @Poller(fixedDelay = "100"))
+  public MessageChannel outChannel() {
+    return new DirectChannel();
+  }
+}
