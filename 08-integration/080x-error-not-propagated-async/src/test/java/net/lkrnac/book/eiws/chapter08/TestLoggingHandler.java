ce082594437cdd3b34cf7510efca80c82cc96628
lubos krnac <lubos.krnac@gmail.com>
2015-08-16 20:28:48 +0100

    Remove unused file

--- 08-integration/080x-error-not-propagated-async/src/test/java/net/lkrnac/book/eiws/chapter08/TestLoggingHandler.java
+++
@@ -1,9 +0,0 @@
-package net.lkrnac.book.eiws.chapter08;
-
-import org.springframework.integration.handler.LoggingHandler;
-
-public class TestLoggingHandler extends LoggingHandler {
-  public TestLoggingHandler() {
-    super("INFO");
-  }
-}
