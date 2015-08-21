9696672f0da6ec0ce903915ccf354209ef30d423
lubos krnac <lubos.krnac@gmail.com>
2015-08-21 19:50:05 +0100

    Change add order to programatic message creation example

--- 08-integration/0801-programmatic-message-creation/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/0801-programmatic-message-creation/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -1,6 +1,5 @@
 package net.lkrnac.book.eiws.chapter08;
 
-import lombok.extern.slf4j.Slf4j;
 import net.lkrnac.book.eiws.chapter08.in.SiWrapperServiceWithHeaders;
 
 import org.springframework.boot.SpringApplication;
@@ -10,20 +9,19 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 
-@Slf4j
 @SpringBootApplication
 @ImportResource("classpath:si-config.xml")
 public class SiApplication {
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
 
-    Message<String> message =
-        MessageBuilder.withPayload("simple message")
-            .setHeader("simpleHeader", "simple header").build();
-
     SiWrapperServiceWithHeaders wrapperService =
         ctx.getBean(SiWrapperServiceWithHeaders.class);
+
+    Message<String> message =
+        MessageBuilder.withPayload("simple message")
+            .setHeader("simpleHeader", "simple header").setPriority(10).build();
+
     wrapperService.processMessage(message);
-    log.info(message + " sent");
   }
 }
