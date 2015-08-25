98132abd35f9b083fa93f8ee12947161322106a7
lubos krnac <lubos.krnac@gmail.com>
2015-08-25 22:39:18 +0100

    Change example to better highlight Filter features

--- 08-integration/0815-filter-xml-ref/src/test/java/net/lkrnac/book/eiws/chapter08/SiApplicationTests.java
+++ 08-integration/0815-filter-xml-ref/src/test/java/net/lkrnac/book/eiws/chapter08/SiApplicationTests.java
@@ -2,56 +2,43 @@ package net.lkrnac.book.eiws.chapter08;
 
 import java.io.IOException;
 
+import net.lkrnac.book.eiws.chapter08.in.SiWrapperServiceVoid;
 import net.lkrnac.book.eiws.chapter08.out.TestWriteRepository;
 import net.lkrnac.book.eiws.chapter08.out.WriteRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.springframework.web.client.RestTemplate;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-@WebIntegrationTest("server.port:0")
 @SpringApplicationConfiguration(classes = SiApplication.class)
 public class SiApplicationTests extends AbstractTestNGSpringContextTests {
-  private static final String LOCALHOST = "http://localhost:";
   private static final String MESSAGE_TEXT = "simple message";
 
   {
     System.setProperty("spring.profiles.active", "integration-test");
   }
 
-  @Value("${local.server.port}")
-  private int serverPort;
-
   @Autowired
   private WriteRepository writeRepository;
+
+  @Autowired
+  private SiWrapperServiceVoid wrapperService;
 
   @Test
   public void testSi() throws InterruptedException, IOException {
     // GIVEN
-    RestTemplate restTemplate = new RestTemplate();
-    String url = LOCALHOST + serverPort;
 
     // WHEN
-    String response1 =
-        restTemplate.postForObject(url, MESSAGE_TEXT, String.class);
-    String response2 =
-        restTemplate.postForObject(url, "corruptedMessage", String.class);
-    String response3 =
-        restTemplate.postForObject(url, MESSAGE_TEXT, String.class);
+    wrapperService.processText(MESSAGE_TEXT);
+    wrapperService.processText("corrupted message");
+    wrapperService.processText(MESSAGE_TEXT);
 
     // THEN
     TestWriteRepository testWriteRepository =
         (TestWriteRepository) writeRepository;
     Assert.assertEquals(testWriteRepository.getMessage(), MESSAGE_TEXT);
     Assert.assertEquals(testWriteRepository.getMessage(), MESSAGE_TEXT);
-    Assert.assertEquals(response1, "true");
-    Assert.assertEquals(response2, null);
-    Assert.assertEquals(response3, "true");
-    // THEN
   }
 }
