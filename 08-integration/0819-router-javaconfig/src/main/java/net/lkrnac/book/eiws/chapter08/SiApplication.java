0ae92eb32a6d2c3cf25e1acadb8a8fd8b10ca5eb
lubos krnac <lubos.krnac@gmail.com>
2015-08-26 22:12:11 +0100

    Order example according book

--- 08-integration/0819-router-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/0819-router-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -1,18 +1,23 @@
 package net.lkrnac.book.eiws.chapter08;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lkrnac.book.eiws.chapter08.in.SiWrapperServiceAnnotated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class SiApplication {
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
 
-    SiWrapperServiceAnnotated wrapperService = ctx.getBean(SiWrapperServiceAnnotated.class);
-    wrapperService.processText("message1");
-    wrapperService.processText("message2");
+    SiWrapperServiceAnnotated wrapperService =
+        ctx.getBean(SiWrapperServiceAnnotated.class);
+    boolean result1 = wrapperService.processText("message1");
+    log.info("Result:" + result1);
+    boolean result2 = wrapperService.processText("message2");
+    log.info("Result:" + result2);
   }
 }
