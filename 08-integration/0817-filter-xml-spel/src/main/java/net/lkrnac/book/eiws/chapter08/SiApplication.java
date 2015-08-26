e02ac0715f8d299959be7188be071c9f6da4db1b
lubos krnac <lubos.krnac@gmail.com>
2015-08-26 18:43:29 +0100

    Refactor example to better reflect Filter features

--- 08-integration/0817-filter-xml-spel/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/0817-filter-xml-spel/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -1,13 +1,24 @@
 package net.lkrnac.book.eiws.chapter08;
 
+import net.lkrnac.book.eiws.chapter08.in.SiWrapperServiceVoid;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("classpath:si-config.xml")
 public class SiApplication {
+  private static final String SIMPLE_MESSAGE = "simple message";
+
   public static void main(String[] args) throws InterruptedException {
-    SpringApplication.run(SiApplication.class, args);
+    ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
+
+    SiWrapperServiceVoid wrapperService =
+        ctx.getBean(SiWrapperServiceVoid.class);
+    wrapperService.processText(SIMPLE_MESSAGE);
+    wrapperService.processText("corrupted message");
+    wrapperService.processText(SIMPLE_MESSAGE);
   }
 }
