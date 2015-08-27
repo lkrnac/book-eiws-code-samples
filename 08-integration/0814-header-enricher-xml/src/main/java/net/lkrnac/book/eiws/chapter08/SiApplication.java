9d2e88e9bb2de827c49947b52a6a492311a7d3b3
lubos krnac <lubos.krnac@gmail.com>
2015-08-27 21:02:44 +0100

    Commit edits from book

--- 08-integration/0814-header-enricher-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/0814-header-enricher-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -10,15 +10,12 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @ImportResource("classpath:si-config.xml")
 public class SiApplication {
-  private static final String MESSAGE2 = "message2";
-  private static final String MESSAGE1 = "message1";
-
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
 
     SiWrapperServiceVoid wrapperService =
         ctx.getBean(SiWrapperServiceVoid.class);
-    wrapperService.processText(MESSAGE1);
-    wrapperService.processText(MESSAGE2);
+    wrapperService.processText("message1");
+    wrapperService.processText("message2");
   }
 }
