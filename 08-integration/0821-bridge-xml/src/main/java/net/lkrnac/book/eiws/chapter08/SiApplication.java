736f6d9d51730d933d6a97ed6e02d854fa0274fe
lubos krnac <lubos.krnac@gmail.com>
2015-08-27 19:18:42 +0100

    Reflect chanes from book

--- 08-integration/0821-bridge-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/0821-bridge-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -1,6 +1,5 @@
 package net.lkrnac.book.eiws.chapter08;
 
-import lombok.extern.slf4j.Slf4j;
 import net.lkrnac.book.eiws.chapter08.in.SiWrapperServiceVoid;
 
 import org.springframework.boot.SpringApplication;
@@ -8,21 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
-@Slf4j
 @SpringBootApplication
 @ImportResource("classpath:si-config.xml")
 public class SiApplication {
-  private static final String MESSAGE2 = "message2";
-  private static final String MESSAGE1 = "message1";
-
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
 
     SiWrapperServiceVoid wrapperService =
         ctx.getBean(SiWrapperServiceVoid.class);
-    wrapperService.processText(MESSAGE1);
-    log.info(MESSAGE1 + " sent");
-    wrapperService.processText(MESSAGE2);
-    log.info(MESSAGE2 + " sent");
+    wrapperService.processText("message1");
+    wrapperService.processText("message2");
   }
 }
