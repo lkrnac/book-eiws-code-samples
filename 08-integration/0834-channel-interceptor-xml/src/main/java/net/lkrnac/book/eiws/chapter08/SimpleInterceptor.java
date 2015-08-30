46ea2a19dc2936f169bd6cdb75013d16dd50f184
lubos krnac <lubos.krnac@gmail.com>
2015-08-30 20:19:42 +0100

    Reflect edits from book

--- 08-integration/0834-channel-interceptor-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SimpleInterceptor.java
+++ 08-integration/0834-channel-interceptor-xml/src/main/java/net/lkrnac/book/eiws/chapter08/SimpleInterceptor.java
@@ -12,38 +12,41 @@ import org.springframework.stereotype.Component;
 public class SimpleInterceptor implements ChannelInterceptor {
   @Override
   public Message<?> preSend(Message<?> message, MessageChannel channel) {
-    log.info("Going to send message " + message + " to channel " + channel);
+    log.info("Going to send message {} to channel {}", message.getPayload(),
+        channel);
     return message;
   }
 
   @Override
   public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
-    log.info("Message " + message + " was successfully send to channel "
-        + channel);
+    log.info("Sending of message {} to channel {} finished",
+        message.getPayload(), channel);
   }
 
   @Override
   public void afterSendCompletion(Message<?> message, MessageChannel channel,
       boolean sent, Exception ex) {
-    log.info("Message " + message + " was send to channel " + channel);
+    log.info("Message {} was successfully send to channel {}",
+        message.getPayload(), channel);
   }
 
   @Override
   public boolean preReceive(MessageChannel channel) {
-    log.info("Going to receive message to channel " + channel);
+    log.info("Going to receive message from channel {}", channel);
     return true;
   }
 
   @Override
   public Message<?> postReceive(Message<?> message, MessageChannel channel) {
-    log.info("Message " + message + " was successfully received to channel "
-        + channel);
+    log.info("Receiving of message {} from channel {} finished",
+        message.getPayload(), channel);
     return message;
   }
 
   @Override
   public void afterReceiveCompletion(Message<?> message,
       MessageChannel channel, Exception ex) {
-    log.info("Message " + message + " was received to channel " + channel);
+    log.info("Message {} was successfully received from channel {}",
+        message.getPayload(), channel);
   }
 }
