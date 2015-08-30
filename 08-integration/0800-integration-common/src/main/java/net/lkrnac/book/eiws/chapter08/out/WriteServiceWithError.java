154f7c31ab19fb3263c740171dcd8c20429333a6
lubos krnac <lubos.krnac@gmail.com>
2015-08-30 22:44:18 +0100

    Extract class to common library

--- 08-integration/0800-integration-common/src/main/java/net/lkrnac/book/eiws/chapter08/out/WriteServiceWithError.java
+++ 08-integration/0800-integration-common/src/main/java/net/lkrnac/book/eiws/chapter08/out/WriteServiceWithError.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WriteServiceWithError {
+  public boolean writeAndIndicateSuccess(String message) {
+    throw new IllegalStateException("error occurred");
+  }
+
   public void write(String message) {
     throw new IllegalStateException("error occurred");
   }
