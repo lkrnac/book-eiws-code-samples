9eddc4a5dc33cbeec27bfbc9b1217c62f5043afa
lubos krnac <lubos.krnac@gmail.com>
2015-08-31 22:04:37 +0100

    Reflect edits from book

--- 08-integration/0838-error-not-propagated-async/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 08-integration/0838-error-not-propagated-async/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -1,5 +1,6 @@
 package net.lkrnac.book.eiws.chapter08;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lkrnac.book.eiws.chapter08.in.SiWrapperServiceVoid;
 
 import org.springframework.boot.SpringApplication;
@@ -7,14 +8,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
+@Slf4j
 @SpringBootApplication
 @ImportResource("classpath:si-config.xml")
 public class SiApplication {
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
 
-    SiWrapperServiceVoid wrapperService =
-        ctx.getBean(SiWrapperServiceVoid.class);
-    wrapperService.processText("simple message");
+    try {
+      SiWrapperServiceVoid wrapperService =
+          ctx.getBean(SiWrapperServiceVoid.class);
+      wrapperService.processText("simple message");
+    } catch (IllegalStateException ise) {
+      log.info("Exception thrown from SI flow", ise);
+    }
   }
 }
