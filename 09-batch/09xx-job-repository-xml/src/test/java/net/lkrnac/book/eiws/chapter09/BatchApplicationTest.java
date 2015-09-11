418632059c5a483b3a8a223635d5e4b2b1d42029
lubos krnac <lubos.krnac@gmail.com>
2015-09-11 20:00:59 +0100

    JobRepository example with XMl config

---
+++ 09-batch/09xx-job-repository-xml/src/test/java/net/lkrnac/book/eiws/chapter09/BatchApplicationTest.java
@@ -0,0 +1,46 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import net.lkrnac.book.eiws.chapter09.step.SimpleExecutableStep;
+import net.lkrnac.book.eiws.chapter09.step.TestExecutableStep;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(classes = BatchApplication.class)
+public class BatchApplicationTest extends AbstractTestNGSpringContextTests {
+  {
+    System.setProperty("spring.profiles.active", "integration-test");
+  }
+
+  @Autowired
+  private SimpleExecutableStep executableStep;
+
+  @Autowired
+  private JobLauncher jobLauncher;
+
+  @Autowired
+  private Job job;
+
+  @Test(timeOut = 3000)
+  public void testBatch() throws Exception {
+    // GIVEN - Spring configuration
+
+    // WHEN - Spring Batch job is started automatically
+    JobExecution execution = jobLauncher.run(job, new JobParameters());
+
+    // THEN
+    TestExecutableStep testExecutableStep = (TestExecutableStep) executableStep;
+    Assert.assertEquals(testExecutableStep.getMessage(), "Boil Water");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Tea");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Water");
+    Assert.assertEquals(execution.getStatus(), BatchStatus.COMPLETED);
+  }
+}
