870b7eaf219b7899b3de15d2b960619b3f8457e3
lubos krnac <lubos.krnac@gmail.com>
2015-09-12 16:48:33 +0100

    XML configured Chunk Oriented processing listeners

---
+++ 09-batch/09xx-chunk-listener-xml/src/main/java/net/lkrnac/book/eiws/chapter09/SimpleChunkListener.java
@@ -0,0 +1,35 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import net.lkrnac.book.eiws.chapter09.step.SimpleExecutablePoint;
+
+import org.springframework.batch.core.ChunkListener;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleChunkListener implements ChunkListener {
+  private SimpleExecutablePoint executablePoint;
+
+  @Autowired
+  public SimpleChunkListener(SimpleExecutablePoint executablePoint) {
+    super();
+    this.executablePoint = executablePoint;
+  }
+
+  @Override
+  public void beforeChunk(ChunkContext context) {
+    executablePoint.execute("Starting chunk: " + context);
+  }
+
+  @Override
+  public void afterChunk(ChunkContext context) {
+    executablePoint.execute("After chunk: " + context);
+  }
+
+  @Override
+  public void afterChunkError(ChunkContext context) {
+    executablePoint.execute("Error occured in chunk: " + context);
+  }
+
+}
