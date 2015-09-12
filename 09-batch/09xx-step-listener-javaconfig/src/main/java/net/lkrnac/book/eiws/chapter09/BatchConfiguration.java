734e1e8891422237dabb9c68d1d1eed63c430564
lubos krnac <lubos.krnac@gmail.com>
2015-09-12 10:06:40 +0100

    Java configured StepListener example kick off

---
+++ 09-batch/09xx-step-listener-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter09/BatchConfiguration.java
@@ -0,0 +1,48 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import net.lkrnac.book.eiws.chapter09.step.tea.AddTea;
+import net.lkrnac.book.eiws.chapter09.step.tea.AddWater;
+import net.lkrnac.book.eiws.chapter09.step.tea.BoilWater;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+  @Bean
+  public Step boilWaterStep(StepBuilderFactory stepFactory, BoilWater boilWater) {
+    return stepFactory.get("boilWaterStep").tasklet(boilWater).build();
+  }
+
+  @Bean
+  public Step addTeaStep(StepBuilderFactory stepFactory, AddTea addTea) {
+    return stepFactory.get("addTeaStep").tasklet(addTea).build();
+  }
+
+  @Bean
+  public Step addWaterStep(StepBuilderFactory stepFactory, AddWater addWater) {
+    return stepFactory.get("addWaterStep").tasklet(addWater).build();
+  }
+
+  @Bean
+  public Job prepareTeaJob(JobBuilderFactory jobBuilderFactory,
+      @Qualifier("boilWaterStep") Step boilWaterStep,
+      @Qualifier("addTeaStep") Step addTeaStep,
+      @Qualifier("addWaterStep") Step addWaterStep,
+      TeaJobListener teaJobListener) {
+    return jobBuilderFactory.get("prepareTeaJob")
+        .listener(teaJobListener)
+        .flow(boilWaterStep)
+        .next(addTeaStep)
+        .next(addWaterStep)
+        .end()
+        .build();
+  }
+}
