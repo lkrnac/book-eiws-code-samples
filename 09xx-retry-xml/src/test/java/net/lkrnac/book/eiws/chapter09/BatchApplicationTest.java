0573a23e02b98ba45fb2f010e41160061056e16b
lubos krnac <lubos.krnac@gmail.com>
2015-09-14 20:57:43 +0100

    Add Retry example with XML configuration

---
+++ 09xx-retry-xml/src/test/java/net/lkrnac/book/eiws/chapter09/BatchApplicationTest.java
@@ -0,0 +1,39 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import java.util.stream.Stream;
+
+import net.lkrnac.book.eiws.chapter09.BatchApplication;
+import net.lkrnac.book.eiws.chapter09.write.TestWriteRepository;
+import net.lkrnac.book.eiws.chapter09.write.WriteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = BatchApplication.class)
+public class BatchApplicationTest extends AbstractTestNGSpringContextTests {
+  {
+    System.setProperty("spring.profiles.active", "integration-test");
+  }
+
+  @Autowired
+  private WriteRepository writeRepository;
+
+  @Test(timeOut = 5000)
+  public void testBatch() {
+    // GIVEN - Spring configuration
+
+    // WHEN - Spring Batch job is started automatically
+
+    // THEN
+    TestWriteRepository testWriteRepository =
+        (TestWriteRepository) writeRepository;
+    Stream.iterate(0, idx -> idx + 1)
+        .map(idx -> "simple record " + idx + " processed")
+        .limit(15)
+        .forEach(
+            exp -> Assert.assertEquals(testWriteRepository.getMessage(), exp));
+  }
+}
