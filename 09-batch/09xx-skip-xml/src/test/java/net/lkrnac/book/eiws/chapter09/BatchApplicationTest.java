8219c4c376d15efa7c02ee182b60ef50f55ad1c6
lubos krnac <lubos.krnac@gmail.com>
2015-09-16 20:35:39 +0100

    Add Skip example with XML configuration

---
+++ 09-batch/09xx-skip-xml/src/test/java/net/lkrnac/book/eiws/chapter09/BatchApplicationTest.java
@@ -0,0 +1,38 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import java.util.stream.Stream;
+
+import net.lkrnac.book.eiws.chapter09.write.TestWriteRepository;
+import net.lkrnac.book.eiws.chapter09.write.WriteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = BatchApplication.class)
+public class BatchApplicationTest extends AbstractTestNGSpringContextTests {
+  {
+    System.setProperty("spring.profiles.active", "integration-test");
+  }
+
+  @Autowired
+  private WriteRepository writeRepository;
+
+  @Test(timeOut = 5000)
+  public void testBatch() {
+    // GIVEN - Spring configuration
+
+    // WHEN - Spring Batch job is started automatically
+
+    // THEN
+    TestWriteRepository testWriteRepository =
+        (TestWriteRepository) writeRepository;
+    Stream.iterate(1, idx -> idx + 1)
+        .map(idx -> "simple record " + idx + " processed")
+        .limit(14)
+        .forEach(
+            exp -> Assert.assertEquals(testWriteRepository.getMessage(), exp));
+  }
+}
