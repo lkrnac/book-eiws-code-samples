5e622e77b6baf91269e59ae824d16bcdb4a9c963
lubos krnac <lubos.krnac@gmail.com>
2015-09-17 21:43:14 +0100

    Add Decider example with XML configuration

--- 09-batch/09xx-decision-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
+++ 09-batch/09xx-decision-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
@@ -9,13 +9,17 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @Slf4j
 @SpringBootApplication
+@EnableBatchProcessing
+@ImportResource("classpath:batch-config.xml")
 public class BatchApplication {
   public static void main(String[] args) throws Exception {
     ConfigurableApplicationContext context =
---
+++ 09-batch/09xx-decision-xml/src/test/java/net/lkrnac/book/eiws/chapter09/BatchApplicationTest.java
@@ -0,0 +1,77 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.lkrnac.book.eiws.chapter09.step.SimpleExecutablePoint;
+import net.lkrnac.book.eiws.chapter09.step.TestExecutablePoint;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = BatchApplication.class)
+public class BatchApplicationTest extends AbstractTestNGSpringContextTests {
+  {
+    System.setProperty("spring.profiles.active", "integration-test");
+  }
+
+  @Autowired
+  private SimpleExecutablePoint executableStep;
+
+  @Autowired
+  private Job job;
+
+  @Autowired
+  private JobLauncher jobLauncher;
+
+  @Test(timeOut = 5000)
+  public void testBatchWithIngredient() throws Exception {
+    // GIVEN
+    Map<String, JobParameter> jobParametersMap = new HashMap<>();
+    jobParametersMap.put("teaIngredient", new JobParameter("milk"));
+    JobParameters jobParameters = new JobParameters(jobParametersMap);
+
+    // WHEN
+    JobExecution execution = jobLauncher.run(job, jobParameters);
+
+    // THEN
+    TestExecutablePoint testExecutableStep =
+        (TestExecutablePoint) executableStep;
+    Assert.assertEquals(testExecutableStep.getMessage(), "Boil Water");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Tea");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Water");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Milk");
+    Assert.assertEquals(execution.getStatus(), BatchStatus.COMPLETED);
+  }
+
+  @Test(timeOut = 5000)
+  public void testBatchWithoutIngredient() throws Exception {
+    // GIVEN
+    Map<String, JobParameter> jobParametersMap = new HashMap<>();
+    jobParametersMap.put("teaIngredient", new JobParameter((String) null));
+    JobParameters jobParameters = new JobParameters(jobParametersMap);
+
+    // WHEN
+    JobExecution execution = jobLauncher.run(job, jobParameters);
+
+    // THEN
+    TestExecutablePoint testExecutableStep =
+        (TestExecutablePoint) executableStep;
+    Assert.assertEquals(testExecutableStep.getMessage(), "Boil Water");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Tea");
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Water");
+
+    Assert.assertEquals(testExecutableStep.getMessage(1000), null);
+    Assert.assertEquals(execution.getStatus(), BatchStatus.COMPLETED);
+  }
+
+}
