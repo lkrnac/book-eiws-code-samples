098cb0e0cd2bdabd8aad4a49c47d4d6d7143033b
lubos krnac <lubos.krnac@gmail.com>
2015-09-17 22:25:23 +0100

    Add Job Restart Example with Java config

---
+++ 09-batch/09xx-job-restart-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
@@ -0,0 +1,42 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class BatchApplication {
+  public static void main(String[] args) throws Exception {
+    ConfigurableApplicationContext context =
+        SpringApplication.run(BatchApplication.class, args);
+
+    JobLauncher jobLauncher = (JobLauncher) context.getBean(JobLauncher.class);
+    Job job = (Job) context.getBean("prepareTeaJob");
+
+    JobExecution execution1 = jobLauncher.run(job, new JobParameters());
+    log.info("Exit Status : {}", execution1.getStatus());
+
+    JobExecution execution2 = jobLauncher.run(job, new JobParameters());
+    log.info("Exit Status : {}", execution2.getStatus());
+
+    JobExecution execution3 = jobLauncher.run(job, new JobParameters());
+    log.info("Exit Status : {}", execution3.getStatus());
+
+    Job jobNotRestarteble =
+        (Job) context.getBean("prepareTeaJobNotRestartable");
+
+    JobExecution execution4 =
+        jobLauncher.run(jobNotRestarteble, new JobParameters());
+    log.info("Exit Status : {}", execution4.getStatus());
+
+    context.close();
+
+  }
+}
---
+++ 09-batch/09xx-job-restart-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter09/BatchConfiguration.java
@@ -0,0 +1,71 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import net.lkrnac.book.eiws.chapter09.step.tea.AddTea;
+import net.lkrnac.book.eiws.chapter09.step.tea.AddWater;
+import net.lkrnac.book.eiws.chapter09.step.tea.BoilWater;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.FlowJob;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+  @Bean
+  public Step boilWaterStep(StepBuilderFactory stepFactory, BoilWater boilWater) {
+    return stepFactory.get("boilWaterStep")
+        .tasklet(boilWater)
+        .allowStartIfComplete(true)
+        .build();
+  }
+
+  @Bean
+  public Step addTeaStep(StepBuilderFactory stepFactory, AddTea addTea) {
+    return stepFactory.get("addTeaStep")
+        .tasklet(addTea)
+        .allowStartIfComplete(true)
+        .startLimit(2)
+        .build();
+  }
+
+  @Bean
+  public Step addWaterStep(StepBuilderFactory stepFactory, AddWater addWater) {
+    return stepFactory.get("addWaterStep").tasklet(addWater).build();
+  }
+
+  @Bean
+  public Job prepareTeaJob(JobBuilderFactory jobBuilderFactory,
+      @Qualifier("boilWaterStep") Step boilWaterStep,
+      @Qualifier("addTeaStep") Step addTeaStep,
+      @Qualifier("addWaterStep") Step addWaterStep) {
+    return jobBuilderFactory.get("prepareTeaJob")
+        .flow(boilWaterStep)
+        .next(addTeaStep)
+        .next(addWaterStep)
+        .end()
+        .build();
+  }
+
+  @Bean
+  public Job prepareTeaJobNotRestartable(JobBuilderFactory jobBuilderFactory,
+      @Qualifier("boilWaterStep") Step boilWaterStep,
+      @Qualifier("addTeaStep") Step addTeaStep,
+      @Qualifier("addWaterStep") Step addWaterStep) {
+    FlowJob job =
+        (FlowJob) jobBuilderFactory.get("prepareTeaJobNotRestartable")
+            .flow(boilWaterStep)
+            .next(addTeaStep)
+            .next(addWaterStep)
+            .end()
+            .build();
+    job.setRestartable(false);
+    return job;
+  }
+
+}
