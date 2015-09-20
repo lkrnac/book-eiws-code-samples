08c705cd448d451b71feaa3f7c08e36fbe82bf5c
lubos krnac <lubos.krnac@gmail.com>
2015-09-20 21:40:26 +0100

    Finish Remote partitioning example

---
+++ 09-batch/09xx-remote-partitioning-master/src/main/java/net/lkrnac/book/eiws/chapter09/SimpleInterceptor.java
@@ -0,0 +1,52 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SimpleInterceptor implements ChannelInterceptor {
+  @Override
+  public Message<?> preSend(Message<?> message, MessageChannel channel) {
+    log.info("Going to send message {} to channel {}", message.getPayload(),
+        channel);
+    return message;
+  }
+
+  @Override
+  public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
+    log.info("Sending of message {} to channel {} finished",
+        message.getPayload(), channel);
+  }
+
+  @Override
+  public void afterSendCompletion(Message<?> message, MessageChannel channel,
+      boolean sent, Exception ex) {
+    log.info("Message {} was successfully send to channel {}",
+        message.getPayload(), channel);
+  }
+
+  @Override
+  public boolean preReceive(MessageChannel channel) {
+    log.info("Going to receive message from channel {}", channel);
+    return true;
+  }
+
+  @Override
+  public Message<?> postReceive(Message<?> message, MessageChannel channel) {
+    log.info("Receiving of message {} from channel {} finished",
+        message.getPayload(), channel);
+    return message;
+  }
+
+  @Override
+  public void afterReceiveCompletion(Message<?> message,
+      MessageChannel channel, Exception ex) {
+    log.info("Message {} was successfully received from channel {}",
+        message.getPayload(), channel);
+  }
+}
--- 09-batch/09xx-remote-partitioning-slave/src/main/java/net/lkrnac/book/eiws/chapter09/BatchSlaveApplication.java
+++ 09-batch/09xx-remote-partitioning-slave/src/main/java/net/lkrnac/book/eiws/chapter09/BatchSlaveApplication.java
@@ -3,9 +3,10 @@ package net.lkrnac.book.eiws.chapter09;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @EnableBatchProcessing
 @ImportResource({ "classpath:batch-slave-config.xml",
     "classpath:jms-config.xml" })
