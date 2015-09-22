3f9ffbbdc8a2a87804ab958349726570e50cb3ea
lubos krnac <lubos.krnac@gmail.com>
2015-09-22 21:33:04 +0100

    Add Parallel step example with XML config

---
+++ 09-batch/09xx-parallel-steps-xml/src/test/java/net/lkrnac/book/eiws/chapter09/BatchApplicationTest.java
@@ -0,0 +1,40 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import net.lkrnac.book.eiws.chapter09.step.SimpleExecutablePoint;
+import net.lkrnac.book.eiws.chapter09.step.TestExecutablePoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@SpringApplicationConfiguration(classes = BatchApplication.class)
+public class BatchApplicationTest extends AbstractTestNGSpringContextTests {
+  {
+    System.setProperty("spring.profiles.active", "integration-test");
+  }
+
+  @Autowired
+  private SimpleExecutablePoint executableStep;
+
+  @Test(timeOut = 5000)
+  public void testBatch() {
+    // GIVEN - Spring configuration
+
+    // WHEN - Spring Batch job is started automatically
+
+    // THEN
+    TestExecutablePoint testExecutableStep =
+        (TestExecutablePoint) executableStep;
+    Assert.assertEquals(testExecutableStep.getMessage(), "Boil Water");
+    String concurrentRecord1 = testExecutableStep.getMessage();
+    String concurrentRecord2 = testExecutableStep.getMessage();
+    if ("Add Tea".equals(concurrentRecord1)) {
+      Assert.assertEquals(concurrentRecord2, "Add one spoon of sugar");
+    } else {
+      Assert.assertEquals(concurrentRecord2, "Add Tea");
+    }
+    Assert.assertEquals(testExecutableStep.getMessage(), "Add Water");
+  }
+}
