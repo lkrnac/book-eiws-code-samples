974c328b2389a527989312c4f810554c9d3dbe71
lubos krnac <lubos.krnac@gmail.com>
2015-09-27 16:54:16 +0100

    Reflect edits from book

---
+++ 09-batch/0907-job-launcher-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter09/BatchBeansConfiguration.java
@@ -0,0 +1,31 @@
+package net.lkrnac.book.eiws.chapter09;
+
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.support.AbstractPlatformTransactionManager;
+
+@Configuration
+public class BatchBeansConfiguration {
+  @Bean
+  public AbstractPlatformTransactionManager transactionManager() {
+    return new ResourcelessTransactionManager();
+  }
+
+  @Bean
+  public JobRepository jobRepository(
+      AbstractPlatformTransactionManager transactionManager) throws Exception {
+    return (new MapJobRepositoryFactoryBean(transactionManager)).getObject();
+  }
+
+  @Bean
+  public JobLauncher jobLauncher(JobRepository jobRepository) {
+    SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
+    simpleJobLauncher.setJobRepository(jobRepository);
+    return simpleJobLauncher;
+  }
+}
