9800ed7029969f143b46e6953395a700d95d4e84
lubos krnac <lubos.krnac@gmail.com>
2015-09-28 22:44:01 +0100

    Reflect edits from book

--- 09-batch/0902-chunk-processing-generic-javaconfig/runFromCli.sh
+++ 09-batch/0902-chunk-processing-generic-javaconfig/runFromCli.sh
@@ -1,2 +1,2 @@
-java -Dloader.main=org.springframework.batch.core.launch.support.CommandLineJobRunner -jar target/0902-chunk-processing-generic-javaconfig-0.0.2-SNAPSHOT.jar net.lkrnac.book.eiws.chapter09.BatchApplication simpleRecordsJob
+java -jar target/0902-chunk-processing-generic-javaconfig-0.0.2-SNAPSHOT.jar
 
