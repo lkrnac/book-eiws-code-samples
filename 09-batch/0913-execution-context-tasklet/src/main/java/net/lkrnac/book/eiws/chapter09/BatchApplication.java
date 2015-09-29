9f4d4eea547cb41d295af0211656f45b84256d39
lubos krnac <lubos.krnac@gmail.com>
2015-09-29 22:26:41 +0100

    Reflect edits from book

--- 09-batch/0913-execution-context-tasklet/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
+++ 09-batch/0913-execution-context-tasklet/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
@@ -1,16 +1,12 @@
 package net.lkrnac.book.eiws.chapter09;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
 
-@Slf4j
 @SpringBootApplication
 public class BatchApplication {
   public static void main(String[] args) throws Exception {
@@ -20,15 +16,9 @@ public class BatchApplication {
     JobLauncher jobLauncher = (JobLauncher) context.getBean(JobLauncher.class);
     Job job = (Job) context.getBean("prepareTeaJob");
 
-    JobExecution execution1 = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status : {}", execution1.getStatus());
-
-    JobExecution execution2 = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status : {}", execution2.getStatus());
-
-    JobExecution execution3 = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status : {}", execution3.getStatus());
-
+    jobLauncher.run(job, new JobParameters());
+    jobLauncher.run(job, new JobParameters());
+    jobLauncher.run(job, new JobParameters());
     context.close();
   }
 }
