cbeccb31d6fbc8c14627b70ac071c7e12b8f6f95
lubos krnac <lubos.krnac@gmail.com>
2015-10-01 20:04:21 +0100

    Fix example

--- 09-batch/0917-job-listener-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchConfiguration.java
+++ 09-batch/0917-job-listener-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchConfiguration.java
@@ -1,8 +1,10 @@
 package net.lkrnac.book.eiws.chapter09;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+@Configuration
 @EnableBatchProcessing
 @ImportResource("classpath:batch-config.xml")
 public class BatchConfiguration {
