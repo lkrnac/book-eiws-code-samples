faa98b92c91224201a96c37b7883617ab8a2cf52
lubos krnac <lubos.krnac@gmail.com>
2015-10-03 19:35:34 +0100

    Reflect edits from book

--- 09-batch/0926-step-restart-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
+++ 09-batch/0926-step-restart-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
@@ -1,43 +1,28 @@
 package net.lkrnac.book.eiws.chapter09;
 
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-@Slf4j
 @SpringBootApplication
 public class BatchApplication {
   public static void main(String[] args) throws Exception {
     ConfigurableApplicationContext context =
         SpringApplication.run(BatchApplication.class, args);
-
     JobLauncher jobLauncher = (JobLauncher) context.getBean(JobLauncher.class);
+
     Job job = (Job) context.getBean("prepareTeaJob");
-
-    JobExecution execution;
-
-    execution = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status: {}", execution.getStatus());
-    execution = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status: {}", execution.getStatus());
-    execution = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status: {}", execution.getStatus());
+    jobLauncher.run(job, new JobParameters());
+    jobLauncher.run(job, new JobParameters());
+    jobLauncher.run(job, new JobParameters());
 
     Job jobNotRestarteble =
         (Job) context.getBean("prepareTeaJobNotRestartable");
-
-    execution = jobLauncher.run(jobNotRestarteble, new JobParameters());
-    log.info("Exit Status: {}", execution.getStatus());
-    execution = jobLauncher.run(jobNotRestarteble, new JobParameters());
-    log.info("Exit Status: {}", execution.getStatus());
-
+    jobLauncher.run(jobNotRestarteble, new JobParameters());
+    jobLauncher.run(jobNotRestarteble, new JobParameters());
     context.close();
-
   }
 }
