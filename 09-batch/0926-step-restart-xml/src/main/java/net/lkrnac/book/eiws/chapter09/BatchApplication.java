67b8c3e910dff6c9bfbc0d851ead84d15d93b07a
lubos krnac <lubos.krnac@gmail.com>
2015-10-03 17:54:00 +0100

    Reflect edits from book

--- 09-batch/0926-step-restart-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
+++ 09-batch/0926-step-restart-xml/src/main/java/net/lkrnac/book/eiws/chapter09/BatchApplication.java
@@ -5,17 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
 @Slf4j
 @SpringBootApplication
-@EnableBatchProcessing
-@ImportResource("classpath:batch-config.xml")
 public class BatchApplication {
   public static void main(String[] args) throws Exception {
     ConfigurableApplicationContext context =
@@ -24,21 +20,22 @@ public class BatchApplication {
     JobLauncher jobLauncher = (JobLauncher) context.getBean(JobLauncher.class);
     Job job = (Job) context.getBean("prepareTeaJob");
 
-    JobExecution execution1 = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status : {}", execution1.getStatus());
+    JobExecution execution;
 
-    JobExecution execution2 = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status : {}", execution2.getStatus());
-
-    JobExecution execution3 = jobLauncher.run(job, new JobParameters());
-    log.info("Exit Status : {}", execution3.getStatus());
+    execution = jobLauncher.run(job, new JobParameters());
+    log.info("Exit Status: {}", execution.getStatus());
+    execution = jobLauncher.run(job, new JobParameters());
+    log.info("Exit Status: {}", execution.getStatus());
+    execution = jobLauncher.run(job, new JobParameters());
+    log.info("Exit Status: {}", execution.getStatus());
 
     Job jobNotRestarteble =
         (Job) context.getBean("prepareTeaJobNotRestartable");
 
-    JobExecution execution4 =
-        jobLauncher.run(jobNotRestarteble, new JobParameters());
-    log.info("Exit Status : {}", execution4.getStatus());
+    execution = jobLauncher.run(jobNotRestarteble, new JobParameters());
+    log.info("Exit Status: {}", execution.getStatus());
+    execution = jobLauncher.run(jobNotRestarteble, new JobParameters());
+    log.info("Exit Status: {}", execution.getStatus());
 
     context.close();
 
