71cf44f444b41cfbebc3dc0285654ed07089265e
lubos krnac <lubos.krnac@gmail.com>
2015-10-10 08:40:00 +0100

    Replace Java configuration SI flow
    XML flow allows for shorter book content

--- 0843-messaging-template-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
+++ 0843-messaging-template-javaconfig/src/main/java/net/lkrnac/book/eiws/chapter08/SiApplication.java
@@ -3,12 +3,14 @@ package net.lkrnac.book.eiws.chapter08;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.core.MessagingTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication
+@ImportResource("classpath:si-config.xml")
 public class SiApplication {
   public static void main(String[] args) throws InterruptedException {
     ApplicationContext ctx = SpringApplication.run(SiApplication.class, args);
